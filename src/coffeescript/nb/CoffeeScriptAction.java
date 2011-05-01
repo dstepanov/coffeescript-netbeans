003d657870a7a32c24d6d96832e815ffe4c04f66
Denis Stepanov <denis.stepanov@gmail.com>
2011-05-01 20:31:59 +0200

    Allow apply action on multiple coffee files

--- src/coffeescript/nb/CoffeeScriptAction.java
+++ src/coffeescript/nb/CoffeeScriptAction.java
@@ -1,14 +1,22 @@
 package coffeescript.nb;
 
+import coffeescript.nb.CoffeeScriptCompiler.CompilerResult;
 import java.awt.event.ActionEvent;
-import java.beans.PropertyChangeListener;
-import java.util.List;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.Future;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import org.netbeans.api.progress.ProgressHandle;
 import org.netbeans.api.progress.ProgressHandleFactory;
+import org.netbeans.modules.parsing.api.ParserManager;
+import org.netbeans.modules.parsing.api.ResultIterator;
+import org.netbeans.modules.parsing.api.Source;
+import org.netbeans.modules.parsing.api.UserTask;
+import org.netbeans.modules.parsing.spi.ParseException;
 import org.openide.LifecycleManager;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
@@ -19,6 +27,7 @@ import org.openide.execution.ExecutorTask;
 import org.openide.filesystems.FileObject;
 import org.openide.util.Cancellable;
 import org.openide.util.ContextAwareAction;
+import org.openide.util.Exceptions;
 import org.openide.util.ImageUtilities;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
@@ -40,8 +49,8 @@ import org.openide.windows.InputOutput;
 public class CoffeeScriptAction extends AbstractAction implements ContextAwareAction {
 
     public Action createContextAwareInstance(Lookup actionContext) {
-        final CoffeeScriptDataObject data = actionContext.lookup(CoffeeScriptDataObject.class);
-        if (data == null) {
+        Collection<? extends CoffeeScriptDataObject> data = actionContext.lookupAll(CoffeeScriptDataObject.class);
+        if (data.isEmpty()) {
             return null;
         }
         return new PopupAction(data);
@@ -50,18 +59,68 @@ public class CoffeeScriptAction extends AbstractAction implements ContextAwareAc
     public void actionPerformed(ActionEvent ae) {
     }
 
-    static class PopupAction extends AbstractAction implements Popup {
+    private static class PopupAction extends AbstractAction implements Popup {
 
-        private CoffeeScriptDataObject data;
+        private Collection<? extends CoffeeScriptDataObject> data;
+        boolean switchOn = true, switchOff = true, switchMultipleValues;
 
-        public PopupAction(CoffeeScriptDataObject data) {
+        public PopupAction(Collection<? extends CoffeeScriptDataObject> data) {
             super(NbBundle.getBundle("coffeescript.nb.resources.Bundle").getString("CoffeeScriptAction"));
             this.data = data;
+
+            for (CoffeeScriptDataObject dataObject : data) {
+                boolean status = CoffeeScriptAutocompileContext.get().isEnabled(dataObject.getPrimaryFile());
+                switchOn = switchOn && status;
+                switchOff = switchOff && !status;
+            }
+            switchMultipleValues = !switchOn && !switchOff;
         }
 
         public JMenuItem getPopupPresenter() {
+            String color = switchMultipleValues ? "gray" : (switchOn ? "green" : "black");
+            String label = switchMultipleValues ? "-" : (switchOn ? "on" : "off");
+            String switchActionName = String.format("<html>Autocompile: <b style=\"color: %s\">%s</b>", color, label);
+
             final JMenu menu = new JMenu(this);
-            menu.add(new AutocompileSwitchAction(data));
+            JMenu switchmenu = new JMenu(switchActionName);
+            if (switchMultipleValues || switchOff) {
+                switchmenu.add(new AbstractAction("Turn on") {
+
+                    public void actionPerformed(ActionEvent ae) {
+                        for (CoffeeScriptDataObject dataObject : data) {
+                            CoffeeScriptAutocompileContext.get().enableAutocompile(dataObject.getPrimaryFile());
+                        }
+                        RequestProcessor processor = RequestProcessor.getDefault();
+                        final Future[] futureHolder = new Future[1];
+                        futureHolder[0] = processor.submit(new CompilerTask(data) {
+
+                            public void run() {
+                                try {
+                                    compile();
+                                } catch (Exception e) {
+                                    Exceptions.printStackTrace(e);
+                                }
+                            }
+
+                            public boolean cancel() {
+                                futureHolder[0].cancel(true);
+                                return true;
+                            }
+                        });
+                    }
+                });
+            }
+            if (switchMultipleValues || switchOn) {
+                switchmenu.add(new AbstractAction("Turn off") {
+
+                    public void actionPerformed(ActionEvent ae) {
+                        for (CoffeeScriptDataObject dataObject : data) {
+                            CoffeeScriptAutocompileContext.get().disableAutocompile(dataObject.getPrimaryFile());
+                        }
+                    }
+                });
+            }
+            menu.add(switchmenu);
             menu.addSeparator();
             menu.add(new CompileAction(data));
             return menu;
@@ -71,108 +130,124 @@ public class CoffeeScriptAction extends AbstractAction implements ContextAwareAc
         }
     }
 
-    static class AutocompileSwitchAction extends AbstractAction {
+    private static class CompileAction extends AbstractAction {
 
-        CoffeeScriptDataObject data;
+        Collection<? extends CoffeeScriptDataObject> data;
 
-        public AutocompileSwitchAction(CoffeeScriptDataObject data) {
-            this.data = data;
-            boolean autobuildStatus = CoffeeScriptAutocompileContext.get().isEnabled(data.getPrimaryFile());
-            String color = autobuildStatus ? "green" : "black";
-            String label = autobuildStatus ? "on" : "off";
-            putValue(NAME, String.format("<html>Autocompile: <b style=\"color: %s\">%s</b>", color, label));
-        }
-
-        public void actionPerformed(ActionEvent ae) {
-            if (CoffeeScriptAutocompileContext.get().isEnabled(data.getPrimaryFile())) {
-                CoffeeScriptAutocompileContext.get().disableAutocompile(data.getPrimaryFile());
-            } else {
-                CoffeeScriptAutocompileContext.get().enableAutocompile(data.getPrimaryFile());
-            }
-        }
-    }
-
-    static class CompileAction extends AbstractAction {
-
-        CoffeeScriptDataObject data;
-
-        public CompileAction(CoffeeScriptDataObject data) {
+        public CompileAction(Collection<? extends CoffeeScriptDataObject> data) {
             super("Compile");
             this.data = data;
         }
 
         public void actionPerformed(ActionEvent ae) {
-            final CancelAction cancelAction = new CancelAction();
-            final InputOutput io = IOProvider.getDefault().getIO(data.getPrimaryFile().getNameExt(), new Action[]{
-                        cancelAction});
-
-            final ExecutorTask[] taskHolder = new ExecutorTask[1];
-            Cancellable cancellable = new Cancellable() {
-
-                public boolean cancel() {
-                    taskHolder[0].stop();
-                    cancelAction.setEnabled(false);
-                    return true;
-                }
-            };
-            cancelAction.setCancellable(cancellable);
-            final ProgressHandle handle = ProgressHandleFactory.createHandle("Compiling " + data.getPrimaryFile().getNameExt(), cancellable);
-            taskHolder[0] = ExecutionEngine.getDefault().execute("CoffeeScriptExecutor", new Runnable() {
-
-                public void run() {
-                    handle.start();
-                    try {
-                        cancelAction.setEnabled(true);
-                        LifecycleManager.getDefault().saveAll();
-                        CoffeeScriptRhinoCompiler.CompilerResult result = CoffeeScriptRhinoCompiler.get().compile(data.getPrimaryFile().asText());
-                        if (result == null) {
-                            return; // Canceled
-                        }
-                        if (result.getJs() != null) {
-                            FileObject folder = data.getFolder().getPrimaryFile();
-                            FileObject file = folder.getFileObject(data.getName(), "js");
-                            if (file != null) {
-                                file.delete();
-                            }
-                            file = folder.createData(data.getName(), "js");
-                            file.getOutputStream().write(result.getJs().getBytes());
-                            io.getOut().append(result.getJs());
-                        } else {
-                            io.getErr().append(result.getError().getMessage());
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace(io.getErr());
-                    } finally {
-                        cancelAction.setEnabled(false);
-                        handle.finish();
-                    }
-                }
-            }, io);
+            new ConsoleOutputCompileTask(data).execute();
         }
     }
 
-    static class CancelAction extends AbstractAction {
+    private static class CancelAction extends AbstractAction {
 
         private Cancellable cancellable;
 
-        CancelAction() {
+        private CancelAction(Cancellable cancellable) {
             putValue(Action.SMALL_ICON, ImageUtilities.loadImageIcon("coffeescript/nb/resources/stop.png", false));
             putValue(Action.NAME, "Stop");
             setEnabled(false);
-        }
-
-        public void setCancellable(Cancellable cancellable) {
             this.cancellable = cancellable;
         }
 
         public void actionPerformed(ActionEvent e) {
+            cancellable.cancel();
             setEnabled(false);
-            RequestProcessor.getDefault().post(new Runnable() {
+        }
+    }
 
-                public void run() {
-                    cancellable.cancel();
+    private static class ConsoleOutputCompileTask extends CompilerTask {
+
+        private final CancelAction cancelAction;
+        private ExecutorTask executorTask;
+        private InputOutput io;
+
+        public ConsoleOutputCompileTask(Collection<? extends CoffeeScriptDataObject> data) {
+            super(data);
+            this.cancelAction = new CancelAction(this);
+            this.io = IOProvider.getDefault().getIO(getName(), new Action[]{cancelAction});
+        }
+
+        public void execute() {
+            executorTask = ExecutionEngine.getDefault().execute("CoffeeScriptExecutor", this, io);
+        }
+
+        @Override
+        public void run() {
+            try {
+                cancelAction.setEnabled(true);
+                super.compile();
+            } catch (Exception e) {
+                e.printStackTrace(io.getErr());
+            } finally {
+                cancelAction.setEnabled(false);
+            }
+        }
+
+        @Override
+        protected void handleResult(CompilerResult result) {
+            if (result.getJs() != null) {
+                io.getOut().append(result.getJs());
+            } else {
+                io.getErr().append(result.getError().getMessage());
+            }
+        }
+
+        @Override
+        public boolean cancel() {
+            executorTask.stop();
+            cancelAction.setEnabled(false);
+            return true;
+        }
+    }
+
+    private static abstract class CompilerTask implements Cancellable, Runnable {
+
+        private Collection<? extends CoffeeScriptDataObject> data;
+        private String taskName;
+
+        public CompilerTask(Collection<? extends CoffeeScriptDataObject> data) {
+            this.data = data;
+            taskName = data.size() > 1 ? "Compiling CoffeeScript files" : "Compiling " + data.iterator().next().getPrimaryFile().getNameExt();
+        }
+
+        public void compile() throws Exception {
+            LifecycleManager.getDefault().saveAll();
+            for (CoffeeScriptDataObject dataObject : data) {
+                ProgressHandle handle = handle = ProgressHandleFactory.createHandle("Compiling " + dataObject.getPrimaryFile().getNameExt(), this);
+                try {
+                    handle.start();
+                    CoffeeScriptRhinoCompiler.CompilerResult result = CoffeeScriptRhinoCompiler.get().compile(dataObject.getPrimaryFile().asText());
+                    if (result == null) {
+                        return; // Canceled
+                    }
+                    if (result.getJs() != null) {
+                        FileObject folder = dataObject.getFolder().getPrimaryFile();
+                        FileObject file = folder.getFileObject(dataObject.getName(), "js");
+                        if (file != null) {
+                            file.delete();
+                        }
+                        file = folder.createData(dataObject.getName(), "js");
+                        file.getOutputStream().write(result.getJs().getBytes());
+                    } else {
+                    }
+                    handleResult(result);
+                } finally {
+                    handle.finish();
                 }
-            });
+            }
+        }
+
+        protected void handleResult(CoffeeScriptRhinoCompiler.CompilerResult result) {
+        }
+
+        public String getName() {
+            return taskName;
         }
     }
 }
