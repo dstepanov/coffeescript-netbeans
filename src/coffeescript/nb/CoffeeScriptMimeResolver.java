38307062dc2797cd4b8fb05a64f6b325d8d4e9b3
Denis Stepanov <denis.stepanov@gmail.com>
2012-01-26 13:15:35 +0100

    Recognize 'Cakefile' as coffeescript file

---
+++ src/coffeescript/nb/CoffeeScriptMimeResolver.java
@@ -0,0 +1,25 @@
+package coffeescript.nb;
+
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.MIMEResolver;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Denis Stepanov
+ */
+@ServiceProvider(service = MIMEResolver.class)
+public class CoffeeScriptMimeResolver extends MIMEResolver {
+
+    public CoffeeScriptMimeResolver() {
+        super(CoffeeScriptLanguage.MIME_TYPE);
+    }
+
+    @Override
+    public String findMIMEType(FileObject fo) {
+        if (fo.getExt().equals("coffee") || fo.getNameExt().equals("Cakefile")) {
+            return CoffeeScriptLanguage.MIME_TYPE;
+        }
+        return null;
+    }
+}
